b4b9ae1577bdda3137e4fb3f9f6ac0b45bf0877a
harrington <harrington.ricks@live.com>
2024-01-24 14:40:16 -0500

    added all three cases

--- ust_program_No_3.0.py
+++ ust_program_No_3.0.py
@@ -1,16 +1,15 @@
 import re
 class PassWordChecker:
+    def check_password_validity(passwords):
+        valid_passwords = []
 
-def check_password_validity(passwords):
-    valid_passwords = []
+        # Split the input sequence of passwords
+        password_list = passwords.split(',')
 
-    # Split the input sequence of passwords
-    password_list = passwords.split(',')
+        for password in password_list:
+            # Check the criteria using regular expressions
+            if re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@]).{6,12}$', password):
+                valid_passwords.append(password)
 
-    for password in password_list:
-        # Check the criteria using regular expressions
-        if re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@]).{6,12}$', password):
-            valid_passwords.append(password)
-
-    # Print the valid passwords separated by a comma
-    print(','.join(valid_passwords))
+        # Print the valid passwords separated by a comma
+        print(','.join(valid_passwords))
